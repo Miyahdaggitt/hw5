389d01d5ed4ddaaf72c1bc06f63763e2c6456def
Miyahdaggitt <zhannamiyah@gmail.com>
2019-10-15 17:07:04 +0600

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,11 @@
+public class Hero {
+     int health ;
+     int damage;
+     int superPower;
+
+    public Hero(int health, int damage, int superPower) {
+        this.health = health;
+        this.damage = damage;
+        this.superPower = superPower;
+    }
+}
---
+++ src/Magicaldoor.java
@@ -0,0 +1,19 @@
+public class Magicaldoor {
+
+    public Hero[] returned (){
+        Hero players [] = new Hero[4];
+        Hero Warrior = new Hero(300,40,20);
+        Hero magic = new Hero (300,40,20);
+        Hero mental = new Hero (300,40,20);
+        Hero tank = new Hero (300,40,20);
+        return players ;
+
+
+
+
+
+    }
+
+
+
+}
